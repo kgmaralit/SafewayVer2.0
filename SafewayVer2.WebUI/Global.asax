a4891526acf6c49de77b5c90a0b850994e15e9f3
kgmaralit <kgmaralit@gmail.com>
2021-02-09 18:55:59 +0800

    Add project files.

---
+++ SafewayVer2.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SafewayVer2.WebUI.MvcApplication" Language="C#" %>
---
+++ SafewayVer2.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SafewayVer2.WebUI.Startup))]
+namespace SafewayVer2.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
